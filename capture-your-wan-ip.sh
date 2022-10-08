4a9869959f9636bebd73ede6a93e5056e471d484
Malawarayan <gmalawarayan@gmail.com>
2022-10-08 22:17:42 +0100

    feat(capture-your-wan-ip.sh): update scripts with comments

--- capture-your-wan-ip.sh
+++ capture-your-wan-ip.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #DateToday
-date=`date +"%Y-%m-%d %T"` 
+date=`date +"%Y-%m-%d %T"`
+
+echo "*********** Starting Today - " $date " - Check Of WAN IP ***********"
 
 #IP ADDRESS TODAY
 ip=`curl icanhazip.com`
@@ -9,27 +11,31 @@ ip=`curl icanhazip.com`
 #Reference file that stores current and previus WAN IP Address
 filename="./my-wan-ip.txt"
 
-#Check if the Reference File exists else create one  
-if [-f $filename]
-	then
-#Logic to read the last line of the above and store data in variables with space delimiter
-IFS=' ' read -r yesterdayDate yestedayDateTime ipFromYesterday comments < <(tail -n1 $filename)
-	else 
-touch my-wan-ip.txt
+#Check if the Reference File exists else create one
+if [ -f $filename ]
+then
+	#Logic to read the last line of the above and store data in variables with space delimiter
+	IFS=' ' read -r yesterdayDate yestedayDateTime ipFromYesterday comments < <(tail -n1 $filename)
+else
+	touch my-wan-ip.txt
 
 #Logic to read the last line of the above and store data in variables with space delimiter
-IFS=' ' read -r yesterdayDate yestedayDateTime ipFromYesterday comments < <(tail -n1 $filename)
+	IFS=' ' read -r yesterdayDate yestedayDateTime ipFromYesterday comments < <(tail -n1 $filename)
 fi
 
-echo IP Address From Last Day - $yesterdayDate is $ipFromYesterday
+echo IP Address From MY-WAN-IP.TXT is - $yesterdayDate was $ipFromYesterday
+
+echo IP Address Today - $date is $ip
 
 #Logic to check if the current IP is the same from the Previous Day
 if [ "$ip" == "$ipFromYesterday" ]
         then
-                echo TRUE - IP HAS NOT CHANGED
+                echo INFO - IP HAS NOT CHANGED
 		echo ${date} ${ip} IP HAS NOT CHANGED >> my-wan-ip.txt
-        else 
-                echo FALSE - IP HAS CHANGED
+		echo "*********** END OF TODAY - " $date " RUN ********************** "
+        else
+                echo WARNING - IP HAS CHANGED
 		echo "Hello From Raspberry PI" | mail -s "WAN IP Has CHANGED" gmalawarayan@gmail.com
                 echo ${date} ${ip} IP HAS CHANGED >> my-wan-ip.txt
+		echo "*********** END OF TODAY - " $date " RUN ********************** "
 fi
