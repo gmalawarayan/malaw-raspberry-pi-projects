8708cbfc490e7e7b81620689d6eed3fa1bb550d1
malaw-hmrc <malawarayan.ganesan@digital.hmrc.gov.uk>
2023-09-15 22:57:33 +0100

    feat(speedTest.py): update documentation in code for better understanding

--- internet-speed-monitor/speedTest.py
+++ internet-speed-monitor/speedTest.py
@@ -4,15 +4,20 @@ import subprocess
 import time
 from influxdb import InfluxDBClient
 
+# Run speedTest 
 response = subprocess.Popen('speedtest --secure', shell=True, stdout=subprocess.PIPE, text=True).stdout.read()
+
+# Use Regex - re to search for each parameter we need to store
 ping = re.search(':\s+(.*?)\s', response, re.MULTILINE)
 download = re.search('Download:\s+(.*?)\s', response, re.MULTILINE)
 upload = re.search('Upload:\s+(.*?)\s', response, re.MULTILINE)
 
+# Take a value from these Regexed values
 ping = ping.group(1)
 download = download.group(1)
 upload = upload.group(1)
 
+# format data as JSON
 speed_data = [
     {
         "measurement" : "internet_speed",
@@ -27,10 +32,11 @@ speed_data = [
     }
 ]
 
-client = InfluxDBClient('localhost', 8086, 'speedmonitor', 'inter@123', 'internetspeed')
-
+# Connect to influxDB, insert password as per your password setting
+client = InfluxDBClient('localhost', 8086, 'speedmonitor', '<password>', 'internetspeed')
+# Write to DB
 client.write_points(speed_data)
-
+# Write it to a File as a Backup
 try:
     f = open('./speedtest.csv', 'a+')
     if os.stat('./speedtest.csv').st_size == 0:
